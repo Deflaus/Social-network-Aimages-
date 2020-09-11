afebfd817330ad68e74d27dbc21d6845a1657a3c
Deflaus <snow41521@gmail.com>
2020-09-12 00:32:56 +0300

    Add ALLOWED_HOSTS

--- mysocnet_photos/settings.py
+++ mysocnet_photos/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'aimages.herokuapp.com',
+]
 
 
 # Application definition
